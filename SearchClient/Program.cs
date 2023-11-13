b08ae628fbdb069ed28a1f0a60b7f84b1ea9ab4e
Santiago Jimenez <santiagocj0212@gmail.com>
2023-11-13 08:51:15 -0500

    Commit Code Search Client

--- SearchClient/Program.cs
+++ SearchClient/Program.cs
@@ -6,10 +6,50 @@ using System.Threading.Tasks;
 
 namespace SearchClient
 {
-    internal class Program
+    public class Program
     {
+        static List<Cliente> cliente = new List<Cliente>
+        {
+            new Cliente { Id = 1, User = "Santiago Jimenez"},
+            new Cliente { Id = 2, User = "Harold Montejo " },
+            new Cliente { Id = 3, User = "Alejandro Ruiz" },
+            new Cliente { Id = 4, User = "Stiven Santiago" }
+        };
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Buscar Cliente");
+            Console.Write("Ingrese el nombre del cliente: ");
+            string searchTerm = Console.ReadLine().ToLower();
+
+            List<Cliente> resultados = BuscarClientePorNombre(searchTerm);
+
+            MostrarResultados(resultados);
+
+            Console.ReadLine();
+        }
+        static List<Cliente> BuscarClientePorNombre(string searchTerm)
+        {
+            List<Cliente> resultados = cliente.FindAll(cliente =>
+                cliente.User.ToLower().Contains(searchTerm)
+            );
+
+            return resultados;
+        }
+        static void MostrarResultados(List<Cliente> resultados)
+        {
+            Console.WriteLine("\nResultados:");
+            if (resultados.Count > 0)
+            {
+                foreach (var cliente in resultados)
+                {
+                    Console.WriteLine($"ID: {cliente.Id}, Nombre: {cliente.User}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron resultados.");
+            }
         }
     }
 }
